1c8c1cb12e66e40ed0dad0a9856f6699114179a2
Rivera-lml <rodrigo.rivera.7186@gmail.com>
2022-04-09 18:15:16 -0600

    modificar

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("nosotros.html")
+    return render_template("ejemplo1.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
